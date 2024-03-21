e87a86370268a0c7e560257e11f7d78a54f103c3
Anna <anka.dubr@gmail.com>
2024-03-21 23:07:06 +0300

    Singleton

---
+++ Singleton/Classes/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace Singleton.Classes
+{
+    internal class Logger
+    {
+        private static Logger? _instance;
+        private static readonly object _lock = new();
+        public readonly List<string> LogEntries = [];
+
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if (_instance is null)
+            {
+                lock (_lock)
+                {
+                    _instance ??= new Logger();
+                }
+            }
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            string logEntry = $"{DateTime.Now}: {message}";
+            LogEntries.Add(logEntry);
+            Console.WriteLine(logEntry);
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using Singleton.Classes;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ЗАДАЧА: 
+            // создать логгер, который будет гарантирует, что информация будет логироваться одинаковым образом
+            // и храниться в единственном выделенном месте месте
+
+            // ИМПЛЕМЕНТАЦИЯ SINGLETON:
+            // приватный к-р, статический метод доступа к объекту, public свойство с записями логов, к-рые могут быть сериализованы или сохранены и переданы иначе
+
+            var logger = Logger.GetInstance();
+            logger.Log("session started");
+            while (true)
+            {
+                logger.Log($"console input: {Console.ReadLine()}");
+            }
+        }
+    }
+}
